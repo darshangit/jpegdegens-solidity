c3ce3454eac0da3814a3eb746d6747530137a597
Darshan Satya <darshans1989@gmail.com>
2022-05-11 08:30:15 -0400

    deploy script

--- scripts/deploy-hello.ts
+++ scripts/deploy-hello.ts
@@ -1,14 +1,19 @@
 import "@nomiclabs/hardhat-ethers";
 import { ethers } from "hardhat";
 
-async function deploy() {
+async function foo() {
   const HelloWorld = await ethers.getContractFactory("HelloWorld");
   const hello = await HelloWorld.deploy();
   await hello.deployed();
-
   return hello;
 }
 
+async function deploy() {
+  const hello = await foo();
+  return hello;
+}
+
+//@ts-ignore
 async function sayHello(hello) {
   console.log("Say hello:", await hello.hello());
 }
