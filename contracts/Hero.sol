74be8b48647ca5b51a85c919fc6007b3c96c95a2
Darshan Satya <darshans1989@gmail.com>
2022-05-15 21:28:05 -0400

    Initial Hero game + test

---
+++ contracts/Hero.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.8.0;
+
+/*
+I want to be paid... 0.05 eth per hero!
+I should be able to get my heroes I have generated.
+Heroes should be stored on the chain.
+stats are strength, health, intellect, magic, dexterity
+stats are randomly generated
+A scale of 1 - 18
+The stats are randomly picked and their amplitude is randomly determined according to the following:
+Stat 1 can max at 18
+Stat 2 can max at 17
+Stat 3 can max at 16
+...
+You could imagine these being an NFT
+They are non divisable*/
+
+contract Hero {
+    enum Class {
+        Mage,
+        Healer,
+        Barbarian
+    }
+
+    function createHero(Class class) public payable {
+        require(msg.value >= 0.05 ether, "Please send more money");
+    }
+}
---
+++ test/Hero.ts
@@ -0,0 +1,33 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+describe("Hero", function () {
+  async function createHero() {
+    const Hero = await ethers.getContractFactory("Hero");
+    const hero = await Hero.deploy();
+    await hero.deployed();
+
+    return hero;
+  }
+
+  let hero;
+
+  before(async function () {
+    hero = await createHero();
+  });
+
+  it("should fail creating hero cause of payment", async function () {
+    let e;
+
+    try {
+      await hero.createHero(0, {
+        value: ethers.utils.parseEther("0.04999999"),
+      });
+    } catch (err) {
+      e = err;
+    }
+
+    expect(e.message.includes("Please send more money")).to.equal(true);
+  });
+});
